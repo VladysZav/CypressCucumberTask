3ca622aaa643f7263c258e29b8b886a21286ce67
Vladyslav <108288355+VladysZav@users.noreply.github.com>
2022-10-19 09:57:30 +0300

    Update Telnyx.js

--- cypress/e2e/Telnyx.js
+++ cypress/e2e/Telnyx.js
@@ -1,25 +1,21 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { MainPage } from "../Pages/mainPage.cy";
+const mainPage = new MainPage();
 
 Given ("Open Telnyx page", () => {
-    const mainPage = new MainPage()
         mainPage.navigate()
         mainPage.acceptButtonClick()   
 });
 When ("Click header button {string}", (nameButton) => {
-    const mainPage = new MainPage()
         mainPage.clickHeaderButton(nameButton);
 });
 When ("Click main button {string}", (nameButton) => {
-    const mainPage = new MainPage()
         mainPage.clickMainButton(nameButton);
 });
 When ("Click main button {string} in Powerful products box", (nameButton) => {
-    const mainPage = new MainPage()
         mainPage.clickMainButtonInPowerfulProducts(nameButton);
 });
 When ("Click footer button {string}", (nameButton) => {
-    const mainPage = new MainPage()
         mainPage.clickFooterButton(nameButton);
 });
 Then ("Check what went to {string} and go back", (namePage) => {
